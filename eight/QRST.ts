826b20a1e522ec04716c1089653d6d2d85929074
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 19:01:29 +0800

    Update QRST.ts

--- eight/QRST.ts
+++ eight/QRST.ts
@@ -1,8 +1,4 @@
-declare module Q{}
-declare module R{}
-declare module S{}
-declare module T{}
-export const Q = '\|/'
-export const R = '/|\''
-export const S = '///'
-export const T = '\\\'' 
+Q = '\|/'
+R = '/|\''
+S = '///'
+T = '\\\'' 
