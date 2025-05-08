610c297f998fa873e03c18213c47b91c0d73ae8a
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 19:01:57 +0800

    Update UVW1W2.ts

--- eight/UVW1W2.ts
+++ eight/UVW1W2.ts
@@ -1,8 +1,4 @@
-declare module U{}
-declare module V{}
-declare module W1{}
-declare module W2{}
-export const U = '+'
-export const V = '-'
-export const W1 = '*'
-export const W2 = '%' 
+U = '+'
+V = '-'
+W1 = '*'
+W2 = '%' 
