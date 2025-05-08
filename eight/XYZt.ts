d854e3b3e19ff6d89bc3a1c57061adfd80540d05
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 19:02:18 +0800

    Update XYZt.ts

--- eight/XYZt.ts
+++ eight/XYZt.ts
@@ -1,8 +1,4 @@
-declare module X{}
-declare module Y{}
-declare module Z{}
-declare module t{}
-export const X = '<>'
-export const Y = '[]'
-export const Z = '()'
-export const t = '{}' 
+X = '<>'
+Y = '[]'
+Z = '()'
+t = '{}' 
