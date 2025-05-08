9ea0345a8c1fb3abaebcf9362ab88d2ba32e25fe
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 18:59:59 +0800

    Update ABCD.ts

--- eight/ABCD.ts
+++ eight/ABCD.ts
@@ -1,8 +1,4 @@
-declare module A{}
-declare module B{}
-declare module C{}
-declare module D{}
-export const A = '|\/|'
-export const B = '|/\|'
-export const C = '|\|'
-export const D = '|/|' 
+A = '|\/|'
+B = '|/\|'
+C = '|\|'
+D = '|/|' 
