0eb0645060af9c572ace4cbc2bc0222e708fcedc
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 19:00:44 +0800

    Update IJKL.ts

--- eight/IJKL.ts
+++ eight/IJKL.ts
@@ -1,8 +1,4 @@
-declare module I{}
-declare module J{}
-declare module K{}
-declare module L{}
-export const I = '/\''
-export const J = '\/'
-export const K = '/\/'
-export const L = '\/\'' 
+I = '/\''
+J = '\/'
+K = '/\/'
+L = '\/\'' 
