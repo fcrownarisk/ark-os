dc5ae928bb1fe3e0353090a18fd1784597c38dfc
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 19:08:44 +0800

    Update jpyramid.js

--- seven/ijklmn/jpyramid.js
+++ seven/ijklmn/jpyramid.js
@@ -1,8 +1,7 @@
-void function JpyramJd(){
+void function Jpyramid(){
     const JpyramJd_6 = [6][7][8][9][0]
     const JpyramJd_7 = [7][8][9][0][6]
     const JpyramJd_8 = [6][7][8][9][0]
     const JpyramJd_9 = [7][8][9][0][6]
     const JpyramJd_0 = [6][7][8][9][0]
-        return JpyramJd
     }
