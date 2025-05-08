b0b542b4737006655f177943eff9d118cc1ab4f4
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-08 19:01:11 +0800

    Update MNOP.ts

--- eight/MNOP.ts
+++ eight/MNOP.ts
@@ -1,8 +1,4 @@
-declare module M{}
-declare module N{}
-declare module O{}
-declare module P{}
-export const M = '//'
-export const N = '\\'
-export const O = '//\''
-export const P = '\//' 
+M = '//'
+N = '\\'
+O = '//\''
+P = '\//' 
